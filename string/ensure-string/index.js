a28383c73f80cc59407d45c8f546b6d9084c4aa4
Achmad Fatah Ibrahim <acfatah@gmail.com>
2024-03-30 14:35:18 +0800

    Add string ensureString method

---
+++ string/ensure-string/index.js
@@ -0,0 +1,9 @@
+export const ensureString = value => {
+  if (value === null) return ''
+
+  if (typeof value !== 'string') {
+    throw new TypeError('Expected argument to be of type string')
+  }
+
+  return value
+}
---
+++ string/ensure-string/index.spec.js
@@ -0,0 +1,32 @@
+import { describe, it, expect } from 'vitest'
+import { ensureString } from '.'
+
+describe('ensureString', () => {
+  it('should turn null into an empty string', () => {
+    const result = ensureString(null)
+
+    expect(result).toBe('')
+  })
+
+  const notAStringValues = [
+    { type: 'undefined', value: undefined },
+    { type: 'number', value: 123 },
+    { type: 'symbol', value: Symbol('a symbol as an object key') },
+    { type: 'array', value: [] },
+    { type: 'set', value: new Set() },
+    { type: 'boolean', value: true },
+    { type: 'object', value: {} },
+    { type: 'function', value: () => {} }
+  ]
+
+  notAStringValues.forEach(({ type, value }) => {
+    it(`"${type}" should throw TypeError`, () => {
+      const expectEnsureString = expect(() => {
+        ensureString(value)
+      })
+
+      expectEnsureString.toThrow(TypeError)
+      expectEnsureString.toThrow(/Expected argument to be of type string/)
+    })
+  })
+})
