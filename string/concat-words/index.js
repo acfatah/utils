b5144e419be8399538136f235c886f33c5cf7418
Achmad Fatah Ibrahim <acfatah@gmail.com>
2024-04-02 15:33:22 +0800

    Add string concatWords method

---
+++ string/concat-words/index.js
@@ -0,0 +1,31 @@
+import { ensureString } from '../ensure-string'
+
+const removeNonWordChars = input => input.replace(/[^a-zA-Z0-9]+/g, ' ')
+const trimString = input => input.trim()
+
+const escapedRegExp = string => string.replace(/[.*+?^${}()|[\]\\]/g, '$&')
+
+const replaceSpacesWithSeparator = (input, separator) => {
+  const escapedDolar =
+    separator[separator.length - 1] === '$' ? separator + '$' : escapedRegExp(separator)
+
+  return input.replace(/\s+/g, escapedDolar)
+}
+
+/**
+ * @param {string} string
+ * @param {string} [separator='_']
+ * @param {Function} [transformer=null]
+ * @returns {string}
+ */
+export const concatWords = (string, separator = '_', transformer = undefined) => {
+  let result = ensureString(string)
+  result = removeNonWordChars(result)
+  result = trimString(result)
+
+  if (transformer && typeof transformer === 'function') result = transformer(result)
+
+  result = replaceSpacesWithSeparator(result, separator)
+
+  return result
+}
---
+++ string/concat-words/index.spec.js
@@ -0,0 +1,59 @@
+import { describe, it, expect } from 'vitest'
+import { concatWords } from '.'
+
+describe('concatWords', () => {
+  const withoutSeparatorInputMaps = [
+    { input: null, expected: '' },
+    { input: 'lorem ipsum', expected: 'lorem_ipsum' },
+    { input: 'lorem123!@# Ipsum', expected: 'lorem123_Ipsum' }
+  ]
+
+  withoutSeparatorInputMaps.forEach(({ input, expected }) => {
+    it(`should expect "${expected}" from "${input}" without separator`, () => {
+      expect(concatWords(input)).toBe(expected)
+    })
+  })
+
+  const inputMaps = [
+    { input: null, separator: '_', expected: '' },
+    { input: '', separator: '_', expected: '' },
+    { input: 'lorem Ipsum', separator: '_', expected: 'lorem_Ipsum' },
+    { input: 'lorem ipsum', separator: '-', expected: 'lorem-ipsum' },
+    {
+      input: 'Lorem ipsum dolor sit amet.',
+      separator: '-',
+      expected: 'Lorem-ipsum-dolor-sit-amet'
+    },
+    {
+      input: 'Lorem ipsum dolor sit amet.',
+      separator: ', ',
+      expected: 'Lorem, ipsum, dolor, sit, amet'
+    },
+    { input: 'FOO BAR', separator: '', expected: 'FOOBAR' },
+    { input: 'FOO BAR', separator: '-', expected: 'FOO-BAR' },
+    { input: 'FOO   BAR', separator: '_', expected: 'FOO_BAR' },
+    { input: 'lorem_Ipsum', separator: '-', expected: 'lorem-Ipsum' },
+    { input: 'lorem123 Ipsum', separator: '', expected: 'lorem123Ipsum' },
+    { input: 'lorem bar-dolor', separator: '-', expected: 'lorem-bar-dolor' },
+    { input: 'lorem bar-dolor', separator: '_', expected: 'lorem_bar_dolor' },
+    { input: 'lorem bar_dolor', separator: '_', expected: 'lorem_bar_dolor' },
+    { input: '$lorem123 !IpsumDolor -sit', separator: '-', expected: 'lorem123-IpsumDolor-sit' },
+    { input: 'lorem ipsum', separator: '$$', expected: 'lorem$$ipsum' },
+    { input: 'lorem ipsum', separator: '++', expected: 'lorem++ipsum' },
+    { input: 'lorem ipsum', separator: '--', expected: 'lorem--ipsum' }
+  ]
+
+  inputMaps.forEach(({ input, separator, expected }) => {
+    it(`should expect "${expected}" from "${input}" with separator "${separator}"`, () => {
+      expect(concatWords(input, separator)).toBe(expected)
+    })
+  })
+
+  it('should runs transformer when provided', () => {
+    const input = 'Lorem Ipsum Dolor Sit amet.'
+    const expected = 'lorem-ipsum-dolor-sit-amet'
+    const transformer = string => string.toLowerCase()
+
+    expect(concatWords(input, '-', transformer)).toBe(expected)
+  })
+})
