8dfb39b8537418be03541a8c0ba7a97525305936
Achmad Fatah Ibrahim <acfatah@gmail.com>
2024-08-14 04:53:32 +0800

    DEPRECATED: Remove addSpaceBeforeCapitalLetter (deprecated since v1, removed in v2, use toSpaceCase instead)

--- lib/string/add-space-before-capital-letter/README.md
+++
@@ -1,3 +0,0 @@
-# `add-space-before-capital-letter`
-
-Add a space before a capital letter.
---
+++ types.d.ts
@@ -0,0 +1,3 @@
+export function concatWords(string: string, separator?: string, transformer?: Function): string
+export function toCamelCase(string: string): string
+export function toPascalCase(string: string): string
